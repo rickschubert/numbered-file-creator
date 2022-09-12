de44fa34c7ab3a1751c7ccb6691e5dbcfb941138
Rick Schubert <rickschubert@gmx.de>
2022-09-12 21:51:16 +0100

    Create dummy scenes

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::process::exit;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Error: You neeed to provide at least one argument, a dynamic path to the new scene file that should be created!");
+        exit(0);
+    }
+
+    let ref new_scene_path = args[1];
+    // dbg!(args);
+    dbg!(new_scene_path);
+}
