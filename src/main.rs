9eccbd4e36b449de00ed946f1d1746512f534629
Rick Schubert <rickschubert@gmx.de>
2022-09-13 23:44:54 +0100

    I now have the files that need renaming

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,9 @@ fn main() {
                         println!("{}", leading_number_from_file);
                         // return true;
                         let leading_number_as_int: &i32 = &leading_number_from_file.parse().unwrap();
-                        return leading_number_from_file.ne(number) && leading_number_as_int >= &number.parse::<i32>().unwrap();
+                        dbg!(leading_number_from_file.eq(number));
+                        dbg!(leading_number_as_int < &number.parse::<i32>().unwrap());
+                        return leading_number_from_file.eq(number) || leading_number_as_int > &number.parse::<i32>().unwrap();
                     }
                 })
                 .into_iter()
