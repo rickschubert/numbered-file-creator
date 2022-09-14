c885ad080b4e259a3f809ddb7a100fd35ee5a2d4
Rick Schubert <rickschubert@gmx.de>
2022-09-14 10:42:39 +0100

    a list of strings, incredible

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use filters::filter::Filter;
 use regex::Regex;
 use std::env;
+use std::ffi::OsString;
 use std::fs::{self, DirEntry};
 use std::fs::{File, ReadDir};
 use std::path::Path;
@@ -18,6 +19,30 @@ fn get_leading_number_from_file(file_name: &str) -> &str {
     return number;
 }
 
+fn filter_for_files_to_be_renamed(inner: ReadDir, number: &str) {
+    let items_that_need_renaming: Vec<Result<DirEntry, std::io::Error>> = inner
+    .filter(|x| match x {
+        Err(_) => false,
+        Ok(dir_entry) => {
+            let n = dir_entry.file_name();
+            let leading_number_from_file =
+                get_leading_number_from_file(n.to_str().unwrap());
+            let leading_number_as_int: &i32 =
+                &leading_number_from_file.parse().unwrap();
+            return leading_number_from_file.eq(number)
+                || leading_number_as_int > &number.parse::<i32>().unwrap();
+        }
+    })
+    .into_iter()
+    .collect();
+    dbg!(&items_that_need_renaming);
+
+    let results: Vec<String> = items_that_need_renaming.into_iter().map(|x| {
+        let n = x.unwrap().file_name().into_string().unwrap();
+        return n;
+    }).collect();
+    dbg!(results);
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -43,23 +68,7 @@ fn main() {
 
     match paths {
         Ok(inner) => {
-            let items_that_need_renaming: Vec<Result<DirEntry, std::io::Error>> = inner
-                .filter(|x| match x {
-                    Err(_) => false,
-                    Ok(dir_entry) => {
-                        let n = dir_entry.file_name();
-                        let leading_number_from_file =
-                            get_leading_number_from_file(n.to_str().unwrap());
-                        let leading_number_as_int: &i32 =
-                            &leading_number_from_file.parse().unwrap();
-                        return leading_number_from_file.eq(number)
-                            || leading_number_as_int > &number.parse::<i32>().unwrap();
-                    }
-                })
-                .into_iter()
-                .collect();
-            dbg!(items_that_need_renaming);
-
+            filter_for_files_to_be_renamed(inner, &number);
             // For each item that needs renaming, increase the number indicator
         }
         Err(error) => panic!("Problem reading the directory: {:?}", error),
